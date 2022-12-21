da163155b1e5fe1b845574c3beec1cc195bca79e
SHIVAM SHELKE <103065763+shvmshlk7@users.noreply.github.com>
2022-12-21 16:26:22 +0530

    Create PTS.cpp

---
+++ PTS.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include <stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+struct Rectangle
+{
+    int length;
+    int breadth;
+};
+
+int main()
+{
+    struct Rectangle *p;
+    p=new Rectangle;
+    
+    p->length=15;
+    p->breadth=7;
+    
+    cout<<p->length<<endl;
+    cout<<p->breadth<<endl;
+    
+    return 0;
+}
